d7a7a39d6219371a114ece39d80a25951ed1a064
juliayee <jye13@calpoly.edu>
2023-03-13 19:46:36 -0700

    define offsets and lengths

--- mytar.c
+++ mytar.c
@@ -15,7 +15,44 @@
 #define setx 0x4
 #define setv 0x2
 #define setS 0x1
+
 #define BLOCKSIZE 512
+
+/*OFFSETS*/
+#define OFF_NAME 0
+#define OFF_MODE 100
+#define OFF_UID 108
+#define OFF_GID 116
+#define OFF_SIZE 124
+#define OFF_MTIME 136
+#define OFF_CHKSUM 148
+#define OFF_TYPEFLAG 156
+#define OFF_LINKNAME 157
+#define OFF_MAGIC 257
+#define OFF_VERSION 263
+#define OFF_UNAME 265
+#define OFF_GNAME 297
+#define OFF_DEVMAJOR 329
+#define OFF_DEVMINOR 337
+#define OFF_PREFIX 345
+
+/*LENGTHS*/
+#define LEN_NAME 100 /*NUL terminated if it fits*/
+#define LEN_MODE 8
+#define LEN_UID 8
+#define LEN_GID 8
+#define LEN_SIZE 12
+#define LEN_MTIME 12
+#define LEN_CHKSUM 8
+#define LEN_TYPEFLAG 1
+#define LEN_LINKNAME 100 /*NUL terminated if it fits*/
+#define LEN_MAGIC 6 /*must be "ustar", NUL-terminated */
+#define LEN_VERSION 2 /*must be "00"*/
+#define LEN_UNAME 32
+#define LEN_GNAME 32
+#define LEN_DEVMAJOR 8
+#define LEN_DEVMINOR 8
+#define LEN_PREFIX 155
 
 void handle_args(int, char **, int *);
 
