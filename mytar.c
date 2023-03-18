5d94e3ec745f305707968a0393de28644dd46092
juliayee <jye13@calpoly.edu>
2023-03-18 00:31:46 -0700

    creat errors updated

--- mytar.c
+++ mytar.c
@@ -13,6 +13,8 @@
 #include <pwd.h>
 #include <dirent.h>
 #include <time.h>
+#include <wait.h>
+#include <string.h>
 
 #define BUFFSIZE 10
 #define FILES 100
@@ -60,15 +62,6 @@
 #define LEN_DEVMINOR 8
 #define LEN_PREFIX 155
 
-typedef struct tar *TarPtr;
-typedef struct tar {
-    char* files[FILES]; /*list of files*/
-    TarHeaderPtr headers[FILES]; 
-    int numFiles; /*number of files*/
-    int numHeaders; /*number of headers in tar*/
-    char *tarName;
-} Tar;
-
 typedef struct tarHeader *TarHeaderPtr;
 typedef struct tarHeader{
     char name[LEN_NAME];
@@ -88,6 +81,16 @@ typedef struct tarHeader{
     char devminor[LEN_DEVMINOR];
 } TarHeader; 
 
+typedef struct tar *TarPtr;
+typedef struct tar {
+    char* files[FILES]; /*list of files*/
+    TarHeaderPtr headers[FILES]; 
+    int numFiles; /*number of files*/
+    int numHeaders; /*number of headers in tar*/
+    char *tarName;
+} Tar;
+
+int dirfd(DIR *dirp);
 /*---MY FUNCTIONS---*/
 TarPtr handle_args(int argc, char **argv, char **opt, int vs[]);
 void fill_files(int argc, char **argv, TarPtr ti);
@@ -103,33 +106,30 @@ void extract(TarPtr tar, int argc, char **argv, char *opt);
 
 int main(int argc,char *argv[]){
     char *options;
-    int vs[1] = {0, 0};
+    int vs[1] = {0};
     /*{V, S}*/
     TarPtr ti;
     int i;
 
     ti = handle_args(argc, argv, &options, vs);
+    switch((int)options) {
+        case 'c':
+            create(ti, vs);
+            break;
+        
+        case 't':
+            if (vs[V] == 1)
+                listV(ti);
+            else           
+                listA(ti); 
+            break;
 
-    for (i = 0; i < strlen(options); i++) {
-        switch((options)[i]) {
-            case 'c':
-                create(ti, vs);
-                break;
-            
-            case 't':
-                if (vs[V] == 1)
-                    listV(ti);
-                else           
-                    listA(ti); 
-                break;
+        case 'x':
+            extract(ti, argc, argv, options);
+            break;
 
-            case 'x':
-                extract(ti, argc, argv, options);
-                break;
-
-            default:
-                break;
-        }
+        default:
+            break;
     }
 
     free(ti);
@@ -201,7 +201,9 @@ void create(TarPtr ti, int *vs){
         dir = opendir(ti->files[i]);
         /*if dir not null*/
         if(dir){
-            comp = strcmp(ti->files[i][strlen(ti->files[i]) - 1], "/");
+            if(ti->files[i][strlen(ti->files[i]) - 1] == '/'){
+                comp = 0;
+            }
             /*if '/'*/
             if(comp == 0){
                 /*copy into buffer*/
@@ -213,12 +215,12 @@ void create(TarPtr ti, int *vs){
                 sprintf(buff, "%s/", ti->files[i]);
             }
             /*Create directory*/
-            createDirectory(ti, buff, dir, ftar);
+            createDirectory(ti, buff, dir, ftar, vs);
             closedir(dir);
         }
         else{
             /*create a file, dir is null*/
-            createFile(ti, ti->files[i]);
+            createFile(ti, buff, ftar, vs);
         }
     }
     close(ftar);
@@ -250,7 +252,7 @@ void createFile(TarPtr t, char *filename, int ftar, int *vs){
         return;
     }
     /*open file*/
-    if(fd = open(filename, O_RDONLY) != 0){
+    if((fd = open(filename, O_RDONLY)) != 0){
         close(fd);
         return;
     }
@@ -279,7 +281,7 @@ void createDirectory(TarPtr t, char *filename, DIR *dir, int ftar, int *vs){
     struct dirent *pd;
     struct stat *sbuff;
     int fdir;
-    char buff[BUFFER];
+    char *buff;
 
     sbuff = calloc(1, (sizeof(struct stat) + 1));
     fdir = dirfd(dir);
@@ -295,7 +297,7 @@ void createDirectory(TarPtr t, char *filename, DIR *dir, int ftar, int *vs){
                 perror("Path too long: createDirectory.");
                 continue;
             }
-            buff = (char*)calloc(PATHMAX, sizeof(char));
+            buff = calloc(PATHMAX, sizeof(char));
             sprintf(buff, "%s%s", filename, pd->d_name);
             new = opendir(buff);
             if(new){
@@ -308,6 +310,7 @@ void createDirectory(TarPtr t, char *filename, DIR *dir, int ftar, int *vs){
             }
         }
     }
+    free(buff);
     free(sbuff);
 }
 
@@ -338,53 +341,6 @@ void writeHeader(TarPtr t, struct stat *sbuff, char *filename, char ty){
     /*linkname, devmajor, devminor*/    
 }
 
-/*-------------------------------Given functions for 
-inserting and removing binary integers 
-from non-conforming headers.----------------------*/
-uint32_t extract_special_int(char *where, int len) {
-    /* For interoperability with GNU tar. GNU seems to
-    * set the high–order bit of the first byte, then
-    * treat the rest of the field as a binary integer
-    * in network byte order.
-    * I don’t know for sure if it’s a 32 or 64–bit int, but for
-    * this version, we’ll only support 32. (well, 31)
-    * returns the integer on success, –1 on failure.
-    * In spite of the name of htonl(), it converts int32 t
-    */
-    int32_t val= −1;
-    if ( (len >= sizeof(val)) && (where[0] & 0x80)) {
-        /* the top bit is set and we have space
-        * extract the last four bytes */
-        val = *(int32_t *)(where+len−sizeof(val));
-        val = ntohl(val); /* convert to host byte order */
-    }
-    return val;
-}
-
-int insert_special_int(char *where, size_t size, int32_t val) {
-    /* For interoperability with GNU tar. GNU seems to
-    * set the high–order bit of the first byte, then
-    * treat the rest of the field as a binary integer
-    * in network byte order.
-    * Insert the given integer into the given field
-    * using this technique. Returns 0 on success, nonzero
-    * otherwise
-    */
-    int err=0;
-    if ( val < 0 || ( size < sizeof(val)) ) {
-        /* if it’s negative, bit 31 is set and we can’t use the flag
-        * if len is too small, we can’t write it. Either way, we’re
-        * done.
-        */
-        err++;
-    } else {
-        /* game on....*/
-        memset(where, 0, size); /* Clear out the buffer */
-        *(int32_t *)(where+size−sizeof(val)) = htonl(val); /* place the int */
-        *where |= 0x80; /* set that high–order bit */
-    }
-    return err;
-}
 /* ------------------------------ CREATE END ------------------------------ */
 
 /* ------------------------------ LIST START ------------------------------ */
